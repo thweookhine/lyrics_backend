658c89189f0377726087691900a22d031ff6a926
thweookhine <mathweookhine@gmail.com>
2025-03-18 22:14:50 +0630

    fix for artists

--- controllers/ArtistController.js
+++ controllers/ArtistController.js
@@ -61,13 +61,67 @@ const deleteArtistById = async(req,res) => {
 }
 
 const addSearchCount = async (req,res) => {
+    const id = req.params.id;
+    if(!id) {
+        return res.status(400).json({error: "ID is required!"})
+    }
 
+    try {
+        const artist = await Artist.findById(id);
+        if(!artist) {
+            return res.status(400).json({error: "No Artist found!"})
+        }
+
+        artist.searchCount = artist.searchCount + 1;
+        await artist.save();
+        return res.status(200).json({})
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
 }
 
-const getAllArtists = async(req,res) => {
+const getArtistById = async (req,res) => {
+    const id = req.params.id;
+
     try {
-        const artistList = await Artist.find().sort({searchCount: -1});
-        return res.status(200).json({artistList})
+        const artist = await Artist.findById(id);
+        if(!artist) {
+            return res.status(400).json({error: "No Artist Found!"})
+        }
+
+        return res.status(200).json(artist)
+    } catch (err) {
+        return res.status(500).json({error: err.msg})
+    }
+}
+
+const searchArtists = async (req, res) => {
+
+    const keyword = req.query.keyword;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 20;
+    const skip = (page - 1) * limit;
+
+    let query = {}
+
+    if(keyword) {
+        query = {
+            $or: [
+                {name: {$regex: keyword, $options: "i"}}
+            ]
+        };
+    } 
+
+    try {
+        const artists = await Artist.find(query).skip(skip).limit(limit);
+        const totalCount = await Artist.countDocuments(query);
+
+        return res.status(200).json({
+            artists,
+            totalPages: Math.ceil(totalCount / limit),
+            currentPage: page,
+            totalCount
+        })
     } catch(err) {
         return res.status(500).json({error: err.msg})
     }
@@ -82,4 +136,4 @@ const getTopArtists = async (req,res) => {
     }
 }
 
-module.exports = {createArtist, updateArtist, deleteArtistById, getAllArtists, getTopArtists}
+module.exports = {createArtist, updateArtist, deleteArtistById, searchArtists, getArtistById, getTopArtists, addSearchCount}
--- routes/artistRouter.js
+++ routes/artistRouter.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const { createArtist, updateArtist, deleteArtistById, getAllArtists, getTopArtists } = require('../controllers/ArtistController');
+const { createArtist, updateArtist, deleteArtistById, getAllArtists, getTopArtists, searchArtists, addSearchCount, getArtistById } = require('../controllers/ArtistController');
 const { validateArtist } = require('../middleware/artistValidation');
 const { authenticateUser } = require('../middleware/authenticateUser');
 const checkRole = require('../middleware/checkRole');
@@ -10,6 +10,9 @@ const artistRouter = express.Router();
 artistRouter.post('/createArtist', validateArtist, authenticateUser, checkRole(['admin']), createArtist)
 artistRouter.put('/updateArtist/:id', validateArtist, authenticateUser, checkRole(['admin']), updateArtist)
 artistRouter.delete('/deleteArtist/:id', validateArtist, authenticateUser, checkRole(['admin']), deleteArtistById)
-artistRouter.get('/', getAllArtists);
+artistRouter.get('/search', searchArtists);
+artistRouter.get('/addSearchCount/:id', addSearchCount)
+artistRouter.get('/getArtistById/:id', getArtistById)
+
 artistRouter.get('/getTopArtists', getTopArtists)
 module.exports = artistRouter
