1119f733891b385e70184ed2579169ef867d1597
thweookhine <mathweookhine@gmail.com>
2025-05-04 15:32:22 +0630

    Add Artist Creation shell

--- dummyDataCreation/ArtistCreation.sh
+++ dummyDataCreation/ArtistCreation.sh
@@ -4,15 +4,18 @@ SHELL_NAME=$(basename "$0")
 EXIT_CODE=0
 EXIT_ERR=1
 
-SAMPLE_COMMAND="bash ${SHELL_NAME} artistList.json"
+SAMPLE_COMMAND="bash ${SHELL_NAME} artistList.json token"
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     echo "Wrong Command Format! Correct Format: bash ${SHELL_NAME} uname limit"
-    echo "Sample Command: ${SAMPLE_CMD}"
+    echo "Sample Command: ${SAMPLE_COMMAND}"
     exit ${EXIT_ERR}
 fi
 
-for row in $(jp -c '.[]' $1); do
-  curl -X POST -H "Content-Type: application/json"\
-  -d "{\"name\": \"${row.name}\"}", \
+jq -c '.artists[]' "$1" | while read -r row; do
+  name=$(echo "$row" | jq -r '.name')
+  type=$(echo "$row" | jq -r '.type')
+  curl -X POST -H "Authorization: Bearer ${2}" -H "Content-Type: application/json"\
+  -d "{\"name\": \"${name}\", \"type\": \"${type}\" }" \
+        http://localhost:3000/api/artists/createArtist
 done
