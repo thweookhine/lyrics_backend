8e0d4b2d404626c71c0d51b9403fa89bcfcae16c
thweookhine <mathweookhine@gmail.com>
2025-07-27 16:40:45 +0630

    Fix Issue #15 and #16

--- utils/Constants.js
+++ utils/Constants.js
@@ -1,9 +1,15 @@
 
 const genreList = [
-    'Pop', 'Rock', 'Hip-Hop', 'Classical', 'Jazz', 'Electronic', 'R&B', 'Country', 'Reggae', 'Blues'
+    'Pop', 'Rock', 'Hip-Hop', 'Classical', 'Jazz', 'Electronic', 'R&B', 
+    'Country', 'Reggae', 'Blues', 'Rock N Roll', 'None'
 ]
 
-const keyList = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+// const keyList = ["A","B","C","D","E","F","G","Ab","Bb","Db","Eb","Gb","Am","Bm","Cm","Dm","Em","Fm","Gm","Abm","Bbm","Dbm","Ebm","Gbm"]
+// const keyList = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+const keyList = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
+    "Ab", "Bb", "Db", "Eb", "Gb", "Am", "Bm", "Cm", "Dm", "Em", "Fm", "Gm", "Abm", 
+    "Bbm", "Dbm", "Ebm", "Gbm", "None"]
 
 const tierList = [0,1,2]
 
